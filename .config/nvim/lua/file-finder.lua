e5fded3be6512135765df7985f987e53ff103db7
David Bauer <davbauer@ucdavis.edu>
2023-09-15 00:03:47 +0200

    Added live grep key bind for telescope

--- .config/nvim/lua/file-finder.lua
+++ .config/nvim/lua/file-finder.lua
@@ -1,2 +1,3 @@
 -- search files, even hidden ones
 vim.keymap.set('n', '<leader>ff', ':lua require"telescope.builtin".find_files({hidden=true})<CR>', {})
+vim.keymap.set('n', '<leader>gg', ':lua require"telescope.builtin".live_grep({hidden=true})<CR>', {})
