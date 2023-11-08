5cfb1984e4c85db1d5957588608ea56a90258c00
David Bauer <davbauer@ucdavis.edu>
2023-11-07 17:19:09 -0800

    Updated i3 config

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,5 +1,6 @@
 # Lock screen to color
-i3lock -c 2f2e42 
+#i3lock -i /home/davidbau/Pictures/Wallpapers/wallhaven-jx1gxy.png # -c 2f2e42
+i3lock -c 2f2e42
 
 # If still locked after a mninute, turn off screen
-sleep 10 && pgrep i3lock && xset dpms force off
+sleep 30 && pgrep i3lock && xset dpms force off
