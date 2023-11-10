31f0338db34a357b3d502561ad672b37b7a1c9b5
David Bauer <davbauer@ucdavis.edu>
2023-11-09 19:40:46 -0800

    Fixed some colors and added keymap to jump to urgent workspace

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,6 +1,6 @@
 # Lock screen to color
 #i3lock -i /home/davidbau/Pictures/Wallpapers/wallhaven-jx1gxy.png # -c 2f2e42
-i3lock -c 2f2e42
+i3lock -c 523d64
 
 # If still locked after a mninute, turn off screen
 sleep 10 && pgrep i3lock && xset dpms force off
